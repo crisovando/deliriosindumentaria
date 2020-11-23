3ebc6d1e90ff3c2969b886f324fdb2b3be38e8a8
Cristian Ovando <cristian.f.ovando@gmail.com>
2020-11-23 16:47:17 -0300

    change meta

--- components/meta.js
+++ components/meta.js
@@ -4,10 +4,10 @@ import { HOME_OG_IMAGE_URL } from '@/lib/constants'
 export default function Meta() {
   return (
     <Head>
-      <meta charset="UTF-8" />
+      <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-      <title>Tailwind Starter Template - Nordic Shop: Tailwind Toolbox</title>
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      <title>Ecommerce Delirios</title>
       <meta name="description" content="Free open source Tailwind CSS Store template" />
       <meta name="keywords" content="tailwind,tailwindcss,tailwind css,css,starter template,free template,store template, shop layout, minimal, monochrome, minimalistic, theme, nordic" />
 
